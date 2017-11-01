40a5920a870c4cb8f72d036b6f85c9d1f2e84065
KaiLiao <kkdyx@vip.qq.com>
2017-11-01 22:18:45 +0800

    :star:初始化项目

---
+++ src/main/java/com/lk/kDeploy/KDeployApplication.java
@@ -0,0 +1,12 @@
+package com.lk.kDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KDeployApplication.class, args);
+	}
+}
