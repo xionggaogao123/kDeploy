835a2ccd9f3e5b645e2c0629a950c1edda8f9716
KaiLiao <kkdyx@vip.qq.com>
2017-11-15 17:27:00 +0800

    增加全局异常捕获

---
+++ src/main/java/com/lk/kDeploy/config/ControllerAdviceConfig.java
@@ -0,0 +1,25 @@
+package com.lk.kDeploy.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lk.kDeploy.base.dto.ResponseDTO;
+import com.lk.kDeploy.constants.ReturnCode;
+import com.lk.kDeploy.util.RespBuildUtil;
+
+@ControllerAdvice
+public class ControllerAdviceConfig {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(ControllerAdviceConfig.class);
+
+    @ExceptionHandler(Exception.class)
+    public @ResponseBody ResponseDTO jsonErrorHandler(HttpServletRequest request, Exception e) {
+        LOGGER.error("有异常！请联系管理员", e);
+        return RespBuildUtil.error(ReturnCode.FAIL);
+    }
+}
